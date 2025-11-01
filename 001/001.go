041b567166477f741295f8ce7ffc2e5fd645300b
cezarltsv <cezarltsv@gmail.com>
2025-11-01 00:22:30 -0300

    feat: basic variable types and declaration

---
+++ 001/001.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+const MAX_LENTH = 100
+var machineName = "Jarvis"
+var ( 
+	isActive bool = true
+	age int 
+	score float64
+)
+
+
+func basicVariables() {
+	var lastName string
+	name := "Naruto"
+	lastName = "Uzumaki"
+
+	fmt.Printf("firstName %s \n", name)
+	fmt.Printf("LastName %s \n", lastName)
+	fmt.Printf("machine name %s \n", machineName)
+	fmt.Printf("isActive: %t, age: %d, score: %f \n", isActive, age, score)
+
+	fmt.Printf("calculation: %d \n", Add(3, 4))
+}
--- 001/main.go
+++ 001/main.go
@@ -1,9 +1,5 @@
 package main
 
-import "fmt"
-
-
 func main() {
-	fmt.Println("heelo amigos")
-	fmt.Println(Add(3, 4))
+	basicVariables()
 }
